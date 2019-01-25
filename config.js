46f190467febb2f11232e4807eee47aac2b3580e
Adrian Scott <2016030032@upsin.edu.mx>
2019-01-24 20:04:02 -0700

    Funciones para modalidad por región en api, y algunas cosas de front

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     'secret': 'YHFBurwaQWdasd*Qdadasd15V$   ',
     //'database': 'mongodb://localhost/donde'
-    'database': 'mongodb+srv://donde:piyOzIQgJmNqFgQd@donde-vz9pi.mongodb.net/donde?retryWrites=true'
+    'database': 'mongodb+srv://donde:piyOzIQgJmNqFgQd@donde-vz9pi.mongodb.net/donde?retryWrites=true',
+    'here':{
+        appId:   'MMuEt8JGOE4df4XvbpgB',
+        appCode: 'VtPulYXY0PbGG9EMIsuzTA'
+    }
 };
